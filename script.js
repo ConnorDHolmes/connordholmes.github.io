76c7b5ad887539da1975b6b031151504e0ba8261
ConnorDHolmes <connordavidholmes@gmail.com>
2024-03-14 01:33:38 -0400

    click actions

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 "use strict";
 
-//MAIN ELEMENTS
-const root = document.documentElement;
-const body = document.body;
-const nav = document.querySelector("nav");
-const startButton = document.getElementById("start-button");
-const hiddenKey = document.querySelector("c-control[hide]");
-const screen = document.querySelector("c-screen");
-
-const liToSections = new Map();
-
-const hoverableEls = [...document.querySelectorAll("[data-h]")];
-
-//PROJECT LIST
-const list = document.querySelector("ul");
-const listEntries = list.querySelectorAll("li");
-const listPairs = new Map();
-const sections = document.querySelectorAll("section");
-
-//RAF SPEED CONTROL
-const fpsBenchmark = 60;
-const frameDurationBenchmark = 1000 / fpsBenchmark;
-let then = document.timeline.currentTime;
-let multiplier = 1;
-
-//use this to fire something every other frame
-let everyOtherFrame = true;
+const root = document.documentElement,
+  //MAIN
+  body = document.body,
+  nav = document.querySelector("nav"),
+  startButton = document.getElementById("start"),
+  resButton = document.getElementById("res"),
+  hiddenKey = document.querySelector("c-control[hide]"),
+  hoverables = [...document.querySelectorAll("[data-h]")],
+  //PROJECT LIST
+  list = document.querySelector("ul"),
+  listEntries = list.querySelectorAll("li"),
+  entryCount = listEntries.length,
+  listPairs = new Map(),
+  //RAF SPEED CONTROL
+  frameDurationBenchmark = 1000 / 60;
+let then = document.timeline.currentTime,
+  everyOtherFrame;
 
 //WINDOW TRAITS
 const win = {
@@ -44,107 +35,107 @@ const win = {
 };
 
 //SCENE
-const sceneEl = document.querySelector("c-scene");
-const scene = {
-  h: sceneEl.offsetHeight,
-  get scale() {
-    return win.h / scene.h;
-  },
-};
+const sceneEl = document.querySelector("c-scene"),
+  scene = {
+    h: sceneEl.offsetHeight,
+    get scale() {
+      return win.h / scene.h;
+    },
+  };
 
 //CURSOR
-const cursorEl = document.querySelector("c-cursor");
-const cursor = {
-  x: {
-    target: win.midX,
-    eased: win.midX,
-  },
-  y: {
-    target: win.midY,
-    eased: win.midY,
-  },
-  half: cursorEl.offsetWidth * 0.5,
-};
+const cursorEl = document.querySelector("c-cursor"),
+  cursor = {
+    x: {
+      target: win.midX,
+      eased: win.midX,
+    },
+    y: {
+      target: win.midY,
+      eased: win.midY,
+    },
+    half: cursorEl.offsetWidth * 0.5,
+  };
 
-//CAMERA CONFIGS
-const cameraPos = new Map();
-cameraPos.set("orbit", { hor: 3, vert: 2, zoom: -864, adj: 90 });
-cameraPos.set("normal", { hor: 95, vert: 50, zoom: 512, adj: 0 });
-cameraPos.set("focused", { hor: 1, vert: 1, zoom: 576, adj: 0 });
+const camera = new Map([
+  ["orbit", { hor: 3, vert: 2, zoom: -864, adj: 90 }],
+  ["normal", { hor: 95, vert: 50, zoom: 512, adj: 0 }],
+  ["focused", { hor: 1, vert: 1, zoom: 576, adj: 0 }],
+]);
 
 //ROOM
-const roomEl = document.querySelector("c-room");
-const room = {
-  range: {
-    mode: "orbit",
-    hor: {
+const roomEl = document.querySelector("c-room"),
+  room = {
+    range: {
+      mode: "orbit",
+      hor: {
+        get target() {
+          return camera.get(room.range.mode).hor;
+        },
+        eased: 3,
+        get cone() {
+          return room.range.hor.eased * 2;
+        },
+        get isInactive() {
+          return Math.abs(this.target - this.eased) < 0.5;
+        },
+      },
+      vert: {
+        get target() {
+          return camera.get(room.range.mode).vert;
+        },
+        eased: 2,
+        get cone() {
+          return room.range.vert.eased * 2;
+        },
+        get isInactive() {
+          return Math.abs(this.target - this.eased) < 0.5;
+        },
+      },
+      zoom: {
+        get target() {
+          return camera.get(room.range.mode).zoom;
+        },
+        eased: -1024,
+        get isInactive() {
+          return Math.abs(this.target - this.eased) < 0.5;
+        },
+      },
+      adj: {
+        get target() {
+          return camera.get(room.range.mode).adj;
+        },
+        eased: 90,
+        get isInactive() {
+          return Math.abs(this.target - this.eased) < 0.5;
+        },
+      },
+    },
+    x: {
       get target() {
-        return cameraPos.get(room.range.mode).hor;
+        return cursor.x.target;
       },
-      eased: 3,
-      get cone() {
-        return room.range.hor.eased * 2;
-      },
-      get isInactive() {
-        return Math.abs(this.target - this.eased) < 0.5;
-      },
+      eased: cursor.x.target,
     },
-    vert: {
+    y: {
       get target() {
-        return cameraPos.get(room.range.mode).vert;
+        return cursor.y.target;
       },
-      eased: 2,
-      get cone() {
-        return room.range.vert.eased * 2;
-      },
-      get isInactive() {
-        return Math.abs(this.target - this.eased) < 0.5;
-      },
+      eased: cursor.y.target,
     },
-    zoom: {
-      get target() {
-        return cameraPos.get(room.range.mode).zoom;
-      },
-      eased: -1024,
-      get isInactive() {
-        return Math.abs(this.target - this.eased) < 0.5;
-      },
+    get pan() {
+      return (
+        -room.range.hor.eased +
+        (room.x.eased / win.w) * room.range.hor.cone +
+        room.range.adj.eased
+      );
     },
-    adj: {
-      get target() {
-        return cameraPos.get(room.range.mode).adj;
-      },
-      eased: 90,
-      get isInactive() {
-        return Math.abs(this.target - this.eased) < 0.5;
-      },
+    get tilt() {
+      return (
+        room.range.vert.eased - (room.y.eased / win.h) * room.range.vert.cone
+      );
     },
-  },
-  x: {
-    get target() {
-      return cursor.x.target;
-    },
-    eased: cursor.x.target,
-  },
-  y: {
-    get target() {
-      return cursor.y.target;
-    },
-    eased: cursor.y.target,
-  },
-  get pan() {
-    return (
-      -room.range.hor.eased +
-      (room.x.eased / win.w) * room.range.hor.cone +
-      room.range.adj.eased
-    );
-  },
-  get tilt() {
-    return (
-      room.range.vert.eased - (room.y.eased / win.h) * room.range.vert.cone
-    );
-  },
-};
+  };
 
 //ALL EASING VALS
 cursor.x.ease = cursor.y.ease = 0.25;
@@ -166,16 +157,12 @@ const easedTraits = [
 
 //ADD CLASS (IF CLASS DOESN'T EXIST)
 function addCl(el, cl) {
-  if (!el.classList.contains(cl)) {
-    el.classList.add(cl);
-  }
+  !el.classList.contains(cl) && el.classList.add(cl);
 }
 
 //REMOVE CLASS (IF CLASS EXISTS)
 function remCl(el, cl) {
-  if (el.classList.contains(cl)) {
-    el.classList.remove(cl);
-  }
+  el.classList.contains(cl) && el.classList.remove(cl);
 }
 
 //TOGGLE CLASS
@@ -185,57 +172,83 @@ function togCl(el, cl) {
 
 //ADD BOOLEAN ATTRIBUTE (IF ATTRIBUTE DOESN'T EXIST)
 function addBl(el, attr) {
-  if (!el.hasAttribute(attr)) {
-    el.setAttribute(attr, "");
-  }
+  !el.hasAttribute(attr) && el.setAttribute(attr, "");
 }
 
 //REMOVE BOOLEAN ATTRIBUTE (IF ATTRIBUTE EXISTS)
 function remBl(el, attr) {
-  if (el.hasAttribute(attr)) {
-    el.removeAttribute(attr);
-  }
+  el.hasAttribute(attr) && el.removeAttribute(attr);
 }
+
+//CLICK ACTIONS
+const actions = new Map();
+actions.set(startButton, () => {
+  addCl(nav, "hide");
+  setTimeout(() => {
+    room.range.mode = "focused";
+    remBl(hiddenKey, "hide");
+    remBl(roomEl, "backface");
+    addCl(nav, "remove");
+  }, 350);
+});
+actions.set(resButton, () => {
+  console.log("resume button clicked");
+});
 
 //CLONE THE PROJECT LIST TO CREATE SEAMLESS WRAPPING
 function cloneListEntries() {
+  const sections = document.querySelectorAll("section");
   listEntries.forEach((entry, index) => {
+    const entryIndex = index;
     const clone = entry.cloneNode(true);
-    hoverableEls.push(clone);
-    listPairs.set(entry, clone);
-    listPairs.set(clone, entry);
-    liToSections.set(entry, sections[index]);
-    liToSections.set(clone, sections[index]);
+    hoverables.push(clone);
+    listPairs.set(entry, clone).set(clone, entry);
+
+    actions
+      .set(entry, () =>
+        sections.forEach((section, index) =>
+          index === entryIndex ? togCl(section, "show") : remCl(section, "show")
+        )
+      )
+      .set(clone, () =>
+        sections.forEach((section, index) =>
+          index === entryIndex + entryCount
+            ? togCl(section, "show")
+            : remCl(section, "show")
+        )
+      );
     list.append(clone);
   });
 }
 
 //CLONE THE SCREEN TO MAKE A REFLECTION
 function cloneScreen() {
+  const screen = document.querySelector("c-screen");
   const reflection = screen.cloneNode(true);
-
-  const screenDescendants = screen.querySelectorAll("*");
   const pairs = new Map();
-  //REFLECTION MUTATION OBSERVER AND CALLBACK
-  function onScreenChange(changes) {
-    changes.forEach((change) => {
-      pairs.get(change.target).classList = change.target.classList;
-    });
-  }
-  const reflectionObserver = new MutationObserver(onScreenChange);
+
+  const observer = new MutationObserver((changes) => {
+    changes.forEach(
+      (change) => (pairs.get(change.target).classList = change.target.classList)
+    );
+  });
 
   //ONLY OBSERVE CHANGES IN ELEMENTS THAT MIGHT CHANGE (MUTABLE OR HOVERABLE)
-  screenDescendants.forEach((descendant, index) => {
-    if (
-      descendant.hasAttribute("data-m") ||
-      descendant.hasAttribute("data-h")
-    ) {
-      pairs.set(descendant, reflection.querySelectorAll("*")[index]);
-      reflectionObserver.observe(descendant, {
+  const reflectionMutables = [
+    ...reflection.querySelectorAll("[data-m], [data-h]"),
+  ];
+  reflectionMutables.forEach((mutable) =>
+    ["data-m", "data-h"].forEach((attr) => mutable.removeAttribute(attr))
+  );
+
+  [...screen.querySelectorAll("[data-m], [data-h]")].forEach(
+    (mutable, index) => {
+      pairs.set(mutable, reflectionMutables[index]);
+      observer.observe(mutable, {
         attributeFilter: ["class"],
       });
     }
-  });
+  );
 
   //list scrolling and looping
   const listClone = pairs.get(list);
@@ -246,7 +259,7 @@ function cloneScreen() {
       parseInt(
         getComputedStyle(list).getPropertyValue("--gap").split("p")[0]
       )) *
-    listEntries.length;
+    entryCount;
 
   list.addEventListener(
     "wheel",
@@ -267,54 +280,38 @@ function cloneScreen() {
 }
 
 //ANIMATION EASING FUNCTION
-function ease(val) {
-  val.eased += (val.target - val.eased) * (val.ease * multiplier);
+function ease(val, mult) {
+  val.eased += (val.target - val.eased) * (val.ease * mult);
 }
 
 //UPDATE HOVERED ELEMENT (TAKING THE CURSOR'S EASING INTO ACCOUNT)
 function updateHoveredEl() {
-  if (!cursorEl.hasAttribute("hide")) {
-    const el = document.elementFromPoint(cursor.x.eased, cursor.y.eased);
-    if (hoverableEls.includes(el)) {
-      hoverableEls.forEach((hoverableEl) => {
-        //account for list entry clones
-        if (hoverableEl === el || hoverableEl === listPairs.get(el)) {
-          addCl(hoverableEl, "hover");
-        } else {
-          remCl(hoverableEl, "hover");
-        }
-      });
-      addBl(cursorEl, "clickable");
-    } else {
-      hoverableEls.forEach((hoverableEl) => {
-        remCl(hoverableEl, "hover");
-      });
-      remBl(cursorEl, "clickable");
-    }
+  const el = document.elementFromPoint(cursor.x.eased, cursor.y.eased);
+  if (hoverables.includes(el)) {
+    hoverables.forEach((h) => {
+      h === el || h === listPairs.get(el)
+        ? addCl(h, "hover")
+        : remCl(h, "hover");
+    });
+    addBl(cursorEl, "clickable");
+  } else {
+    hoverables.forEach((h) => remCl(h, "hover"));
+    remBl(cursorEl, "clickable");
   }
 }
 
 //UPDATES FOR EACH FRAME
 function refresh(timeStamp) {
-  const diff = timeStamp - then;
-  then = timeStamp;
-  multiplier = diff / frameDurationBenchmark;
-
   roomEl.style.transform = `translate3d(0, 0, ${room.range.zoom.eased}px) rotate3d(1, 0, 0, ${room.tilt}deg) rotate3d(0, 1, 0, ${room.pan}deg)`;
   cursorEl.style.transform = `translate3d(${cursor.x.eased}px, ${cursor.y.eased}px, 0)`;
 
-  easedTraits.forEach((trait) => {
-    if (trait.isInactive) {
-      return;
-    }
-    ease(trait);
-  });
+  const multiplier = (timeStamp - then) / frameDurationBenchmark;
+  easedTraits.forEach((trait) => !trait.isInactive && ease(trait, multiplier));
 
-  if (everyOtherFrame) {
-    updateHoveredEl();
-  }
+  everyOtherFrame && !cursorEl.hasAttribute("hide") && updateHoveredEl();
   everyOtherFrame = !everyOtherFrame;
 
+  then = timeStamp;
   requestAnimationFrame(refresh);
 }
 
@@ -356,7 +353,7 @@ root.addEventListener("mouseenter", (e) => {
   cursor.y.eased = e.pageY - cursor.half;
 });
 
-//ALL INPUTS
+//ALL KEY INPUTS
 document.addEventListener("keyup", (e) => {
   if (e.key === "Shift") {
     if (room.range.mode !== "orbit") {
@@ -376,7 +373,6 @@ document.addEventListener("keyup", (e) => {
 //FADE IN SCENE AFTER ALL CONTENT IS LOADED
 document.addEventListener("readystatechange", (e) => {
   if (e.target.readyState === "complete") {
-    //fade out the screen overlay
     remCl(body, "overlay");
   }
 });
@@ -394,30 +390,12 @@ window
 
 //ALL CLICK ACTIONS
 document.addEventListener("click", () => {
-  const canClick = document.querySelector(".hover");
-  if (canClick === startButton) {
-    //enter focused mode from "work button"
-    addCl(nav, "hide");
-    setTimeout(() => {
-      room.range.mode = "focused";
-      remBl(hiddenKey, "hide");
-      remBl(roomEl, "backface");
-      addCl(nav, "remove");
-    }, 350);
-  } else if (liToSections.get(canClick)) {
-    console.log("section!");
-  }
-
-  // sections.forEach((section) => {
-  //   if (section === listItem) {
-  //     togCl(section, "show");
-  //   } else {
-  //     remCl(section, "show");
-  //   }
-  // });
+  const clickableEl = document.querySelector(".hover");
+  actions.get(clickableEl) && actions.get(clickableEl)();
 });
 
 //ON LOAD
+
 //change color mode if needed
 if (window.matchMedia("(prefers-color-scheme: light)").matches) {
   addCl(body, "light-mode");
